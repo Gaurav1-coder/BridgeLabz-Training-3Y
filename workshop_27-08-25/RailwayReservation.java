15edc5cd5bcdeaa0727640b1a7e269303339d57f
gaurav katiyar <katiyargaurav358@gmail.com>
2025-08-27 17:08:44 +0530

    [Gaurav] Added : Add Practice_Problem_27-08-25

--- workshop_27-08-25/RailwayReservation.java
+++ workshop_27-08-25/RailwayReservation.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Practice3 {
+public class RailwayReservation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
