1bf502641a5af4610c6ee56d9925a0c1bdce9ffe
gaurav katiyar <katiyargaurav358@gmail.com>
2025-09-02 15:05:35 +0530

    [Gaurav] Remove : Delete Strings Assignment Question.

---
+++ Strings/Level1-Program/LowerCaseConversion.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class LowerCaseConversion {
+    public static String myToLower(String str) {
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch >= 'A' && ch <= 'Z') {
+                ch = (char)(ch + 32);
+            }
+            result += ch;
+        }
+        return result;
+    }
+
+    public static boolean compareStrings(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter text: ");
+        String str = sc.nextLine();
+
+        String myLower = myToLower(str);
+        String builtInLower = str.toLowerCase();
+
+        System.out.println("Lowercase using charAt(): " + myLower);
+        System.out.println("Lowercase using built-in: " + builtInLower);
+        System.out.println("Are both same? " + compareStrings(myLower, builtInLower));
+    }
+}
